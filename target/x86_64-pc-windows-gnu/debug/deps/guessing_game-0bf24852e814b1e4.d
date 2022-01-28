6fb28a075ebda41c2b8e00dec3f6d8ea22f15d88
Krypion17 <74854681+Krypion17@users.noreply.github.com>
2022-01-28 22:01:40 +0530

    removing windows release

--- target/x86_64-pc-windows-gnu/debug/deps/guessing_game-0bf24852e814b1e4.d
+++
@@ -1,5 +0,0 @@
-/media/krypion17/Drive/Stuff/Code/rust/guessing_game/target/x86_64-pc-windows-gnu/debug/deps/guessing_game-0bf24852e814b1e4.exe: src/main.rs
-
-/media/krypion17/Drive/Stuff/Code/rust/guessing_game/target/x86_64-pc-windows-gnu/debug/deps/guessing_game-0bf24852e814b1e4.d: src/main.rs
-
-src/main.rs:
